2c1b281dfc09e04f6d1b503155efafd0d8d8e271
Avdesh kumar <ku.kumar.avdesh786@gmail.com>
2024-02-11 14:12:23 +0530

    sign-in UI and Functions done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Home from './pages/Home'
-import Signin from './pages/Signin'
+import SignIn from './pages/SignIn'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import SignUp from './pages/SignUp'
@@ -11,7 +11,7 @@ export default function App() {
     <Header/>
     <Routes>
       <Route path='/' element={<Home/>}/>
-      <Route path='/sign-in' element={<Signin/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/profile' element={<Profile/>}/>
