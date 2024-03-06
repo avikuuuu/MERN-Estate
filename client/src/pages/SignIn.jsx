5bb5dee314a4ca3808ca10bd7fb2f8df150b61f9
Avdesh kumar <ku.kumar.avdesh786@gmail.com>
2024-03-06 10:36:56 +0530

    fir 2nd errror

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -5,7 +5,7 @@ import {
   signInStart,
   signInSuccess,
   signInFailure,
-} from '../redux/user/userSlice';
+} from '../redux/user/UserSlice.js';
 import OAuth from '../components/OAuth';
 
 export default function SignIn() {
